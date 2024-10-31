88fe51fa928ebf97ae3924993c1f2728bcd3187b
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-10-30 22:11:57 -0500

    Initial Commit

---
+++ LoginConGoogle/LoginConGoogleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginConGoogleApp.swift
+//  LoginConGoogle
+//
+//  Created by German David Vertel Narvaez on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginConGoogleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoginConGoogleTests/LoginConGoogleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginConGoogleTests.swift
+//  LoginConGoogleTests
+//
+//  Created by German David Vertel Narvaez on 30/10/24.
+//
+
+import Testing
+@testable import LoginConGoogle
+
+struct LoginConGoogleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
